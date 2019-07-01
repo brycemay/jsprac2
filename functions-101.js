54c9361bed431c6478ddc5b5b5eb0ebf40b49a38
Bryce <bryce.may.8@hotmail.com>
2019-07-01 13:37:16 -0600

    functions

---
+++ functions-101.js
@@ -0,0 +1,15 @@
+let greetUser = function () {
+    console.log('Welcome User!')
+}
+
+greetUser()
+greetUser()
+greetUser()
+
+let square = function (num) {
+    let result = num * num
+    return result
+}
+
+let value = square(3)
+square(10)
